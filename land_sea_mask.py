c6bc9913d03987e023f85d0fdcd409ce39719e01
Anna K. <annakatharinakoepke@gmail.com>
2026-03-31 11:25:12 +0200

    Generation of site_locations.py and land_sea_mask.py

---
+++ land_sea_mask.py
@@ -0,0 +1,9 @@
+"""
+A land-sea mask is a binary grid that indicates whether each grid cell is land or sea. 
+In this case it is synthetically generated for testing purposes, with a simple pattern.
+It is not based on real-world data, but serves to demonstrate how to create and 
+use a land-sea mask within the HEP model.
+
+"""
+
+# UNDER CONSTRUCTION
---
+++ site_locations.py
@@ -0,0 +1,8 @@
+""" 
+Site locations, presence and a-priori absence of sites are generated here.
+These site locations are synthetically generated for the purpose of testing the code. 
+In a real application, these would be based on actual data.
+
+"""
+
+# UNDER CONSTRUCTION
